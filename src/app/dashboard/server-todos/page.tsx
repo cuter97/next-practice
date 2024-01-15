64bc4620815f66496127c2b6e201d300c96f3f21
cuter97 <aguscastets@gmail.com>
2024-01-15 18:57:52 -0300

    upload app

---
+++ src/app/dashboard/server-todos/page.tsx
@@ -0,0 +1,28 @@
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
+
+import prisma from "@/lib/prisma";
+import { NewTodo, TodosGrid } from "@/todos";
+
+export const metadata = {
+    title: 'Listado de todos',
+    description: 'SEO Title'
+}
+
+export default async function ServerTodosPage() {
+
+    const todos = await prisma.todo.findMany({ orderBy: { description: 'asc' } });
+
+    return (
+        <>
+            <span className="text-3xl">Server Actions</span>
+        
+            <div>
+                <div className="w-full px-5 mx-5 mb-5 mt-5">
+                    <NewTodo />
+                </div>
+                <TodosGrid todos={todos} />
+            </div>
+        </>
+    );
+}
